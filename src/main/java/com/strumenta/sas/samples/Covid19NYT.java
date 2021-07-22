22c0713755abcd45221adffc7389622fcef3b4cb
Alessio Stalla <alessiostalla@gmail.com>
2021-07-22 15:47:07 +0200

    Updated sas-parser dependency and leveraged new features in Kolasu to simplify Java code.

--- src/main/java/com/strumenta/sas/samples/Covid19NYT.java
+++ src/main/java/com/strumenta/sas/samples/Covid19NYT.java
@@ -6,14 +6,15 @@ import com.strumenta.kolasu.model.TraversingKt;
 import com.strumenta.sas.ast.SasAstBuilder;
 import com.strumenta.sas.parser.SASLexer;
 import com.strumenta.sas.parser.SASParser;
-import kotlin.collections.SetsKt;
 import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
-import java.util.HashSet;
+import java.nio.charset.StandardCharsets;
 
 public class Covid19NYT {
 
@@ -22,7 +23,8 @@ public class Covid19NYT {
     public static void main(String[] args) throws IOException {
         String url = "https://raw.githubusercontent.com/sassoftware/covid-19-sas/master/Data/import-data-nyt.sas";
         try(InputStream inputStream = new URL(url).openStream()) {
-            SASLexer lexer = new SASLexer(CharStreams.fromStream(inputStream));
+            String code = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+            SASLexer lexer = new SASLexer(CharStreams.fromString(code));
             SASParser parser = new SASParser(new CommonTokenStream(lexer));
             SASParser.SourceFileContext sourceFile = parser.sourceFile();
             Node astRoot = new SasAstBuilder().transform(sourceFile, null);
@@ -30,9 +32,9 @@ public class Covid19NYT {
                 for(Node parent = node.getParent(); parent != null; parent = parent.getParent()) {
                     System.out.print(INDENTATION);
                 }
-                System.out.println("Traversing node " + node.getClass().getName() + " @ " + node.getPosition());
+                System.out.println("Traversing node " + node.getClass().getName() + " @ " + node.getPosition() + " with text \"" + StringUtils.abbreviate(node.getPosition().text(code), 30) + "\"");
                 ProcessingKt.processProperties(
-                        node, SetsKt.setOf("parseTreeNode", "position", "specifiedPosition"),
+                        node, ProcessingKt.getDEFAULT_IGNORED_PROPERTIES(),
                         p -> {
                             if(!p.getProvideNodes()) {
                                 for (Node parent = node.getParent(); parent != null; parent = parent.getParent()) {
